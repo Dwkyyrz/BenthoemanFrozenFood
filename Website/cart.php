7026b532235afe6765ee8b993acb1516dd3093a1
riza55358@gmail.com <106018548+Dwkyyrz@users.noreply.github.com>
2022-12-13 13:30:30 +0700

    13/12/2022

--- Website/cart.php
+++ Website/cart.php
@@ -29,7 +29,7 @@ if($_SESSION['status'] !== "login"){
       </div>
 
       <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Products </a></li>
         <li><a href="#">Contact</a></li>
